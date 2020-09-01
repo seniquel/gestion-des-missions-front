f9b726c03be51f4cad40a8df4959217d88135739
Léo SENIQUE <leo.senique@gmail.com>
2020-09-01 10:54:35 +0200

    ajout mvn clean package #27

--- docker-compose.bat
+++ docker-compose.bat
@@ -1,4 +1,9 @@
+echo "### Compilation mvn ###"
+cd ..\gestion-des-missions-back
+mvn clean package
+
 echo "### Compilation npm ###"
+cd ..\gestion-des-missions-front/
 npm run build -- --prod
 
 echo "### Execution docker-compose ###"
