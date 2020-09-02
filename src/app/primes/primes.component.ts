a05217445f360334044cb7f35a8da7773914b9f4
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-09-02 12:13:12 +0200

    ajout tableau primes

--- src/app/primes/primes.component.ts
+++ src/app/primes/primes.component.ts
@@ -1,4 +1,9 @@
+import { missionMock } from './mock.mission';
+import { Mission } from './../missions/miss.domains';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Collegue } from '../auth/auth.domains';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-primes',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimesComponent implements OnInit {
 
-  constructor() { }
+  listeMissions: Mission[];
+  listeDate: Date[];
+  collegueConnecte: Observable<Collegue>;
+
+  constructor(private authSrv: AuthService) {
+    this.listeMissions = missionMock;
+  }
 
   ngOnInit(): void {
+    this.collegueConnecte = this.authSrv.collegueConnecteObs;
+    this.listeDate = this.recupererDate();
+  }
+
+  recupererDate(): Date[] {
+    const datesFin: Date[] = [];
+    for (let i = 0; i < this.listeMissions.length; i++) {
+      datesFin.push(this.listeMissions[i].dateFin);
+    }
+    return datesFin;
   }
 
 }
