2af75840915e9e3b8a9a65ad1df15afc2afe19b5
Léo SENIQUE <leo.senique@gmail.com>
2020-09-02 14:48:23 +0200

    ajout services #7

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Mission } from '../missions/miss.domains';
+import { Observable } from 'rxjs';
+import { Collegue } from '../auth/auth.domains';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  URL_BACKEND = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  recupererMissions(uuid: string): Observable<Mission[]> {
+    return this.http.get<Mission[]>(`${this.URL_BACKEND}/{uuid}/missions`);
+  }
+}
