917353390588388d610ca0d7734cb7a179350e03
JeremYD <delpech.jeremy@hotmail.fr>
2020-09-03 09:45:37 +0200

    USGDM008 - update planning #10

--- src/app/planning-missions/planning-missions.component.ts
+++ src/app/planning-missions/planning-missions.component.ts
@@ -1,10 +1,12 @@
-import { DataService } from './../services/data.service';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { Mission } from './../missions/miss.domains';
 import { DateFormatter } from './date-formatter.provider';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CalendarDateFormatter, CalendarEvent, CalendarView, DAYS_OF_WEEK } from 'angular-calendar';
 import { subYears } from 'date-fns';
+import { Collegue } from '../auth/auth.domains';
+import { AuthService } from '../auth/auth.service';
+import { colors } from './colors-events';
 
 @Component({
   selector: 'app-planning-missions',
@@ -24,23 +26,34 @@ export class PlanningMissionsComponent implements OnInit {
   view: CalendarView = CalendarView.Month;
   viewDate = new Date();
   viewDateNextYear = subYears(new Date(), 1);
-  events: CalendarEvent[] = [];
   locale = 'fr';
   weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
   weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
   CalendarView = CalendarView;
 
+  // Missions dans le calendriers
+  events: CalendarEvent[] = [
+    {
+      title: 'Coucou',
+      color: colors.mission,
+      start: new Date(2020, 8, 4),
+    }
+  ];
+
   // Listes des missions du collègue
-  collegueMission: Observable<Mission>;
+  collegueConnecte: Observable<Collegue>;
+  listeMissions: Mission[] = [];
 
   setView(view: CalendarView) {
     this.view = view;
   }
-
-  constructor(private dataServ: DataService) { }
+  constructor(private authSrv: AuthService) { }
 
   ngOnInit(): void {
-    //this.collegueMission = this.dataServ.recupererMissions();
+    this.collegueConnecte = this.authSrv.collegueConnecteObs;
+    this.collegueConnecte.subscribe(
+      value => this.listeMissions = value.missions
+    );
   }
 
 }
