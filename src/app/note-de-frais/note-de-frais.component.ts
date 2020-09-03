7ece5a4073dbe20756661e558a21f41cc30cddcd
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-09-03 10:09:37 +0200

    ajout corrections

--- src/app/note-de-frais/note-de-frais.component.ts
+++ src/app/note-de-frais/note-de-frais.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Collegue } from '../auth/auth.domains';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { Mission } from '../missions/miss.domains';
 
 @Component({
   selector: 'app-note-de-frais',
@@ -10,10 +11,12 @@ import { AuthService } from '../auth/auth.service';
 })
 export class NoteDeFraisComponent implements OnInit {
   collegueConnecte: Observable<Collegue>;
+  listeMissions: Mission[] = [];
   constructor(private authSrv: AuthService) { }
 
   ngOnInit(): void {
     this.collegueConnecte = this.authSrv.collegueConnecteObs;
+    this.collegueConnecte.subscribe(
+      value => this.listeMissions = value.missions);
   }
-
 }
