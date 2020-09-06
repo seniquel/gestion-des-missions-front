dd21e161a895e6017955b56a7e4c126ac395d3cd
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-09-06 12:06:43 +0200

    début page accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Collegue } from '../auth/auth.domains';
+import { Mission } from '../missions/miss.domains';
+import { AuthService } from '../auth/auth.service';
+import { MissService } from '../missions/miss.service';
 
 @Component({
   selector: 'app-accueil',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  collegueConnecte: Observable<Collegue>;
+  listeMissionsCurrent: Mission[];
+  listeMissionsFutur: Mission[];
+  today: Date = new Date();
+
+  constructor(private authSrv: AuthService, private service: MissService) { }
 
   ngOnInit(): void {
+    this.collegueConnecte = this.authSrv.collegueConnecteObs;
+    this.service.recupererMissionsCurrent().subscribe(
+      mission => {
+        this.listeMissionsCurrent = mission;
+      },
+      err => console.log(err)
+    );
+    this.service.recupererMissionsFutur().subscribe(
+      mission => {
+        this.listeMissionsFutur = mission;
+      },
+      err => console.log(err)
+    );
   }
 
 }
--- src/app/missions/miss.service.ts
+++ src/app/missions/miss.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Mission } from './miss.domains';
 
 /**
  * Service de gestion des missions
@@ -9,6 +12,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MissService {
 
+  URL_BACKEND = environment.baseUrl;
+
   constructor(private http: HttpClient) {
+  }
+
+  recupererMissionsCurrent(): Observable<Mission[]> {
+    return this.http.get<Mission[]>(`${this.URL_BACKEND}missions/current`);
+  }
+
+  recupererMissionsFutur(): Observable<Mission[]> {
+    return this.http.get<Mission[]>(`${this.URL_BACKEND}missions/futur`);
   }
 }
