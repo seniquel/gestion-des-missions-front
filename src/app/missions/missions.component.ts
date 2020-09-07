0bb98fb374d98eba976a6bd3aa1969dd1c936d73
Léo SENIQUE <leo.senique@gmail.com>
2020-09-07 21:11:01 +0200

    fix affichage après suppression #7

--- src/app/missions/missions.component.ts
+++ src/app/missions/missions.component.ts
@@ -28,18 +28,49 @@ export class MissionsComponent implements OnInit {
     this.service.recupererMissions().subscribe(
       value => {
         this.listeMissions = value;
+        this.service.modifListeMissions(this.listeMissions);
       },
-      err => console.log(err),
-      () => { }
+      err => console.log(err)
+    );
+    this.service.sabonnerAListeMission().subscribe(
+      value => {
+        this.listeMissions = value;
+      },
+      err => console.log(err)
     );
   }
 
-  supprimer(uuid: string){
-    this.service.suppprimerMission(uuid).subscribe(
-      () => {},
-      err => console.log(err),
-      () => { }
+  supprimer(mission): void {
+    this.service.suppprimerMission(mission.uuid).subscribe(
+      () => { },
+      err => console.log(err)
     );
+    console.log(this.listeMissions.indexOf(mission));
+    this.listeMissions = this.listeMissions.filter(m => m.uuid !== mission.uuid)
+    this.service.modifListeMissions(this.listeMissions);
+  }
+
+  selectionner(idMission: string): void {
+    this.service.recupererMissionParUuid(idMission).subscribe(
+      mission => this.service.selectionnerMission(mission),
+      err => console.log(err)
+    );
+  }
+
+  transportToString(transport: string): string {
+    switch (transport) {
+      case 'AVION':
+        return 'Avion';
+      case 'TRAIN':
+        return 'Train';
+      case 'COVOITURAGE':
+        return 'Covoiturage';
+      case 'VOITURE_DE_SERVICE':
+        return 'Voiture de service';
+      default:
+        return transport;
+
+    }
   }
 
 }
