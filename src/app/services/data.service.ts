f1c837bc231dff57b4a106aaba4f6f90a79b9d89
Léo SENIQUE <leo.senique@gmail.com>
2020-09-07 21:14:29 +0200

    ajout page modif fonctionnelle #9

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Mission, MissionDto } from '../missions/miss.domains';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Collegue } from '../auth/auth.domains';
 import { Nature } from '../nature-missions/nature.domain';
 
@@ -12,6 +12,8 @@ import { Nature } from '../nature-missions/nature.domain';
 export class DataService {
 
   URL_BACKEND = environment.baseUrl;
+  subjectMissionCourante = new Subject<Mission>();
+  subjectListeMissionCourante = new Subject<Mission[]>();
 
   constructor(private http: HttpClient) { }
 
@@ -27,17 +29,37 @@ export class DataService {
     return this.http.get<Nature[]>(`${this.URL_BACKEND}natures`);
   }
 
-  creerMission(missionCreee: MissionDto): Observable<MissionDto> {
+  creerMission(missionCreee: MissionDto): Observable<Mission> {
     missionCreee.statut = 'INITIALE';
-    return this.http.post<MissionDto>(`${this.URL_BACKEND}collegues/me/missions`, missionCreee);
+    return this.http.post<Mission>(`${this.URL_BACKEND}collegues/me/missions`, missionCreee);
   }
 
-  modifierMission(missionModifiee: MissionDto, uuid: string): Observable<MissionDto> {
+  modifierMission(missionModifiee: MissionDto, uuid: string): Observable<Mission> {
     missionModifiee.statut = 'INITIALE';
-    return this.http.put<MissionDto>(`${this.URL_BACKEND}collegues/me/missions/${uuid}`, missionModifiee);
+    return this.http.put<Mission>(`${this.URL_BACKEND}collegues/me/missions/${uuid}`, missionModifiee);
   }
 
   suppprimerMission(uuid: string): Observable<string> {
     return this.http.delete<string>(`${this.URL_BACKEND}collegues/me/missions/${uuid}`);
+  }
+
+  recupererMissionParUuid(uuid: string): Observable<Mission> {
+    return this.http.get<Mission>(`${this.URL_BACKEND}missions/${uuid}`);
+  }
+
+  selectionnerMission(mission: Mission): void {
+    this.subjectMissionCourante.next(mission);
+  }
+
+  sabonnerAMission(): Observable<Mission> {
+    return this.subjectMissionCourante.asObservable();
+  }
+
+  modifListeMissions(listeMissions: Mission[]): void {
+    this.subjectListeMissionCourante.next(listeMissions);
+  }
+
+  sabonnerAListeMission(): Observable<Mission[]> {
+    return this.subjectListeMissionCourante.asObservable();
   }
 }
